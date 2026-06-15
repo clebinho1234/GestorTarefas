a4687bc6b125e4b1aaabe07448a5aa0b3e198d11
Diogo Silva <73388219+clebinho1234@users.noreply.github.com>
2026-06-15 11:48:04 +0100

    Delete Projeto directory

--- Projeto/logger_config.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(
-    filename="app.log",
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s"
-)
-
-logger = logging.getLogger(__name__)
